ccdb75da8657eb53b66546e9b21b2ee214c8f064
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2024-09-20 16:08:49 +0600

    Update Fibonacci using DP.cpp

--- Fibonacci using DP.cpp
+++ Fibonacci using DP.cpp
@@ -1,22 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int Fibonacci(int n, vector<int>& v) {  // Pass vector by reference
-    if (n == 0 || n == 1) {
-        return n;
-    }
-    if (v[n] != -1) {  // Check if already calculated
+int Fibonacci(int n, vector<int> &v)
+{
+    if (n == 0 or n == 1)
+    {
+        v[n] = n;
         return v[n];
     }
-    v[n] = Fibonacci(n - 1, v) + Fibonacci(n - 2, v);  // Store result
+    else if (v[n] > -1)
+    {
+        return v[n];
+    }
+    v[n] = Fibonacci(n - 1, v) + Fibonacci(n - 2, v);
     return v[n];
 }
 
-int main() {
+int main()
+{
     int n;
     cout << "Enter Fibonacci index:" << endl;
     cin >> n;
-    vector<int> v(n + 1, -1);  // Initialize vector to -1 to represent uncalculated values
-    cout << "Fibonacci is: " << Fibonacci(n, v) << endl;
+    vector<int> v(n + 1, -1);
+    cout << "Fibonacci is:" << Fibonacci(n, v) << endl;
+    cout<<"Fibonacci Row:"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
     return 0;
 }
